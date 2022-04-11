256cb9d752960b7e6a0460f27b1e1f97e456dd02
apsamuel <aaron.psamuel@gmail.com>
2022-04-11 15:11:50 -0400

    feat: runners init

---
+++ dev.js
@@ -0,0 +1,5 @@
+import { Server } from './lib/Server.js';
+
+const server = new Server();
+
+server.repl();
---
+++ server.js
@@ -0,0 +1,6 @@
+import { Server } from './lib/Server.js'
+
+const server = new Server()
+
+
+server.start()
