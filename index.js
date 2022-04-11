71b9bf31e2e9d6809b57779caab380edd2b9df54
apsamuel <aaron.psamuel@gmail.com>
2022-04-11 15:12:13 -0400

    feat: load runners or export

--- index.js
+++ index.js
@@ -1,34 +1,187 @@
-import express from 'express'
-import cors from 'cors'
-import { HomeData } from '@apsamuel/home-data'
+import {
+  join as pathJoin,
+  dirname
+} from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
 
-const app = express()
+import { Server } from './lib/Server.js';
+import { execFile, fork } from 'child_process';
+// import { promisify } from 'util';
 
-app.use(cors())
-app.use(express.json())
+export { Server };
+const controller = new AbortController()
+const __filename = fileURLToPath(import.meta.url)
 
-const data = new HomeData()
 
-app.get('/favicon.ico', (req, res) => {
-  console.log('serving favicon')
-  res.status(204).end()
-})
 
-app.get('/api/health', async (req, res) => {
-  console.log('serving health endpoint')
-  res.json({
-    state: 'running',
-    uptime: process.uptime(),
+if (process.env.NODE_ENV && process.env.NODE_ENV === 'production') {
+  const serverFile = pathJoin(dirname(__filename), 'server.js');
+  console.log(`${JSON.stringify({
+    event: 'load.source',
+    time: new Date().toJSON(),
+    source: serverFile
+  })}`);
+  const { signal } = controller
+  const child = execFile('node', [serverFile], {
+    signal,
+    shell: true
+
+  }, (error) => {
+    console.log(`${JSON.stringify(error)}`) // abort error
   })
-})
 
-app.get('/api/resume', async (req, res) => {
-  console.log('serving resume')
-  res.json(data.resume)
-})
+  child.on('spawn', () => {
+    console.log(`${JSON.stringify({
+      event: 'server.start',
+      time: new Date().toJSON(),
+      pid: child?.pid,
+
+    })}`)
+  })
+
+  child.on('close', (code, signal) => {
+    console.log(`${JSON.stringify({
+      event: 'server.close',
+      time: new Date().toJSON(),
+      pid: child?.pid,
+      code: code || 'no code',
+      signal: signal || 'no signal'
+    })}`)
+  })
+
+  child.on('exit', (code, signal ) => {
+    console.log(
+      `${JSON.stringify({
+        event: 'server.exit',
+        time: new Date().toJSON(),
+        pid: child?.pid,
+        code: code || 'no code',
+        signal: signal || 'no signal',
+      })}`
+    );
+  });
+
+  child.on('disconnect', () => {
+    // console.log(`server.disconnected ${new Date().toJSON()}`)
+    console.log(`${JSON.stringify({
+      event: 'process.detached',
+      time: new Date().toJSON(),
+      pid: child?.pid,
+
+    })}`)
+  })
+
+  child.stdout.on('data', (data) => {
+    // console.log(`server.stdout: ${data.toString()}`)
+    console.log(`${JSON.stringify({
+      event: 'server.stdout',
+      time: new Date().toJSON(),
+      message: data.toString()
+    })}`)
+  })
+
+  child.stderr.on('data', (data) => {
+    console.log(`server.stderr: ${data.toString()}`);
+    console.log(`${JSON.stringify({
+      event: 'server.stderr',
+      time: new Date().toJSON(),
+      message: data.toString()
+    })}`)
+  })
+
+}
 
 
-const port = 8081
-const server = app.listen(port, err => {
-  console[err ? 'error' : 'log'](err || `Server running on ${port}`)
-})
+if (process.env.NODE_ENV && process.env.NODE_ENV === 'development') {
+  const devFile = pathJoin(dirname(__filename), 'dev.js');
+  console.log(
+    `${JSON.stringify({
+      event: 'load.source',
+      time: new Date().toJSON(),
+      source: devFile,
+    })}`
+  );
+  const { signal } = controller;
+
+
+
+  const child = fork(
+    devFile,
+    [],
+    {
+      signal,
+      shell: false,
+      env: { ...process.env }
+    }
+  );
+
+  // console.log(child)
+
+  child.on('spawn', () => {
+    console.log(
+      `${JSON.stringify({
+        event: 'repl.start',
+        time: new Date().toJSON(),
+        pid: child?.pid,
+      })}`
+    );
+  });
+
+  child.on('close', (code, signal) => {
+    console.log(
+      `${JSON.stringify({
+        event: 'repl.close',
+        time: new Date().toJSON(),
+        pid: child?.pid,
+        code: code || 'no code',
+        signal: signal || 'no signal',
+      })}`
+    );
+  });
+
+  child.on('exit', (code, signal) => {
+    console.log(
+      `${JSON.stringify({
+        event: 'repl.exit',
+        time: new Date().toJSON(),
+        pid: child?.pid,
+        code: code || 'no code',
+        signal: signal || 'no signal',
+      })}`
+    );
+  });
+
+  child.on('disconnect', () => {
+    // console.log(`server.disconnected ${new Date().toJSON()}`)
+    console.log(
+      `${JSON.stringify({
+        event: 'repl.detached',
+        time: new Date().toJSON(),
+        pid: child?.pid,
+      })}`
+    );
+  });
+
+  // child.stdout.on('data', (data) => {
+  //   // console.log(`server.stdout: ${data.toString()}`)
+  //   console.log(
+  //     `${JSON.stringify({
+  //       event: 'server.stdout',
+  //       time: new Date().toJSON(),
+  //       message: data.toString(),
+  //     })}`
+  //   );
+  // });
+
+  // child.stderr.on('data', (data) => {
+  //   console.log(`server.stderr: ${data.toString()}`);
+  //   console.log(
+  //     `${JSON.stringify({
+  //       event: 'server.stderr',
+  //       time: new Date().toJSON(),
+  //       message: data.toString(),
+  //     })}`
+  //   );
+  // });
+}
